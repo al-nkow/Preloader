f993821afba22ed4b4bfd38939e27ba2f8abf900
Aleksey Kovalchuk <al.nkow@gmail.com>
2017-11-21 21:11:22 +0300

    169

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
 import '../scss/main.scss';
 
-let s = 300;
-let str = `one more: ${s}`;
-console.log('>>>>>> INIT1', str);
-console.log('>>>>>> INIT2');
+// let s = 300;
+// let str = `one more: ${s}`;
+// console.log('>>>>>> INIT1', str);
+// console.log('>>>>>> INIT2');
+
+window.onload = function() {
+    var preloader = document.getElementsByClassName('preloader')[0];
+    // console.log('>>>>>>', preloader[0].classList);
+    preloader.classList.add('hidden');
+    // console.log('>>>>>> READY');
+}
